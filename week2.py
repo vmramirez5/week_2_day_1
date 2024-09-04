975bbbe237e8cd5ab6671ac91efd28121d3d8711
Ramirez <vmramirez5@cps.k12.il.us>
2024-09-04 15:06:27 -0500

    string slicing

--- week2.py
+++ week2.py
@@ -1,7 +1,11 @@
-#ask someone what day it is
-day = input("What day is it?")
-#ask them their favorite holiday
-holiday = input("What is your favorite holiday?")
-#print out the day and their favorite holiday in a sentence
-print("Today is " + day + " and your favorite holiday is " + holiday)
-#making a change to the file
+# #ask someone what day it is
+# day = input("What day is it?")
+# #ask them their favorite holiday
+# holiday = input("What is your favorite holiday?")
+# #print out the day and their favorite holiday in a sentence
+# print("Today is " + day + " and your favorite holiday is " + holiday)
+# #making a change to the file
+# #string slicing 
+word = "Hello"
+print(word)
+print(word[1:4])
